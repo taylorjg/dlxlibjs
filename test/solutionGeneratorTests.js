f00f17b75564ce86f2dea71b894b5d10563549b5
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-05-25 00:56:28 +0100

    2 spaces

--- test/solutionGeneratorTests.js
+++ test/solutionGeneratorTests.js
@@ -2,41 +2,41 @@ import { expect } from 'chai'
 import { solutionGenerator } from '../lib'
 import * as M from './matrices'
 
-describe('#solutionGenerator tests', function() {
+describe('#solutionGenerator tests', function () {
 
-    it('solving an empty matrix returns an empty generator', function() {
-        const generator = solutionGenerator(M.MATRIX_EMPTY)
-        const solutions = Array.from(generator)
-        expect(solutions).to.be.empty
-    })
+  it('solving an empty matrix returns an empty generator', function () {
+    const generator = solutionGenerator(M.MATRIX_EMPTY)
+    const solutions = Array.from(generator)
+    expect(solutions).to.be.empty
+  })
 
-    it('solving a matrix with one solution returns a generator with one solution', function() {
-        const generator = solutionGenerator(M.MATRIX_WITH_ONE_SOLUTION)
-        const solutions = Array.from(generator)
-        expect(solutions).to.have.lengthOf(1)
-    })
+  it('solving a matrix with one solution returns a generator with one solution', function () {
+    const generator = solutionGenerator(M.MATRIX_WITH_ONE_SOLUTION)
+    const solutions = Array.from(generator)
+    expect(solutions).to.have.lengthOf(1)
+  })
 
-    it('solving a matrix with one solution returns a generator with the correct solution', function() {
-        const generator = solutionGenerator(M.MATRIX_WITH_ONE_SOLUTION)
-        const solutions = Array.from(generator)
-        const solution = solutions[0]
-        expect(solution).to.deep.equal([0, 3, 4])
-    })
+  it('solving a matrix with one solution returns a generator with the correct solution', function () {
+    const generator = solutionGenerator(M.MATRIX_WITH_ONE_SOLUTION)
+    const solutions = Array.from(generator)
+    const solution = solutions[0]
+    expect(solution).to.deep.equal([0, 3, 4])
+  })
 
-    it('solving a matrix with three solutions returns a generator with three solutions', function() {
-        const generator = solutionGenerator(M.MATRIX_WITH_THREE_SOLUTIONS)
-        const solutions = Array.from(generator)
-        expect(solutions).to.have.lengthOf(3)
-    })
+  it('solving a matrix with three solutions returns a generator with three solutions', function () {
+    const generator = solutionGenerator(M.MATRIX_WITH_THREE_SOLUTIONS)
+    const solutions = Array.from(generator)
+    expect(solutions).to.have.lengthOf(3)
+  })
 
-    it('solving a matrix with three solutions returns a generator with the correct three solutions', function() {
-        const generator = solutionGenerator(M.MATRIX_WITH_THREE_SOLUTIONS)
-        const solutions = Array.from(generator)
-        expect(solutions).to.deep.include.members([[0, 3, 4]])
-        expect(solutions).to.deep.include.members([[1, 2]])
-        expect(solutions).to.deep.include.members([[2, 4, 5]])
-    })
+  it('solving a matrix with three solutions returns a generator with the correct three solutions', function () {
+    const generator = solutionGenerator(M.MATRIX_WITH_THREE_SOLUTIONS)
+    const solutions = Array.from(generator)
+    expect(solutions).to.deep.include.members([[0, 3, 4]])
+    expect(solutions).to.deep.include.members([[1, 2]])
+    expect(solutions).to.deep.include.members([[2, 4, 5]])
+  })
 
-    // TODO: add test(s) re onSearchStep
-    // TODO: add test(s) re onSolutionFound
+  // TODO: add test(s) re onSearchStep
+  // TODO: add test(s) re onSolutionFound
 })
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const path = require('path')
 const libDir = path.join(__dirname, 'lib')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: libDir,
-        filename: 'index.js',
-        library: 'dlxlib',
-        libraryTarget: 'umd',
-        // https://medium.com/@JakeXiao/window-is-undefined-in-umd-library-output-for-webpack4-858af1b881df
-        // https://github.com/webpack/webpack/issues/6522
-        globalObject: 'this'
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
-        ]
-    }
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: libDir,
+    filename: 'index.js',
+    library: 'dlxlib',
+    libraryTarget: 'umd',
+    // https://medium.com/@JakeXiao/window-is-undefined-in-umd-library-output-for-webpack4-858af1b881df
+    // https://github.com/webpack/webpack/issues/6522
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
 }
