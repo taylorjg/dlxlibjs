f0daff8d52d504ee6d2c34c0844f6781cb4398c1
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-05-28 11:34:27 +0100

    Add rollup-plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,17 @@
+const { terser } = require('rollup-plugin-terser')
+
 module.exports = {
   input: 'src/index.js',
   output: {
     file: 'lib/index.js',
     format: 'umd',
-    name: 'dlxlib',
-    compact: true
-  }
+    name: 'dlxlib'
+  },
+  plugins: [
+    terser({
+      output: {
+        comments: "all"
+      }
+    })
+  ]
 }
