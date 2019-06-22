1f2757a5c0eabf9d73be81edb048c2f07eb04350
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-06-22 09:05:55 +0100

    Tweaks to update-docs.sh

--- update-docs.sh
+++ update-docs.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-(cd "$DIR" && npm run docs)
+(cd "$DIR" && rm -rf docs && npm run docs)
 
 GH_PAGES_DIR=$DIR/../dlxlibjs-gh-pages
 
 cd "$GH_PAGES_DIR"
 
 git checkout gh-pages
-git rm -rf *
+git rm -rf --ignore-unmatch *
 SUBDIR=`ls "$DIR/docs/dlxlib"`
 echo SUBDIR=$SUBDIR
 cp -R "$DIR/docs/dlxlib/${SUBDIR}/" .
